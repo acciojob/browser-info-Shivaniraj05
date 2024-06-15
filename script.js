34e98f5f9fd0b3599a4b2cbef2fbc9a332f942af
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-06-15 20:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let browserInfo = document.querySelector('#browser-info');
+
+function displayBrowserHistory(){
+    const browserName = navigator.appName;
+    const version = navigator.appVersion;
+
+    const msg = `You are using ${browserName} version ${version}`;
+    browserInfo.textContent = msg;
+}
+
+displayBrowserHistory();
